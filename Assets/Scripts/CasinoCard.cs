096f669e0e3f03504723c7c436966e67edad4799
Marvel Johnson <MarvelJohnson@hotmail.com>
2023-07-29 08:21:29 -0700

    Wrote Implementation For CasinoCard

--- Assets/Scripts/CasinoCard.cs
+++ Assets/Scripts/CasinoCard.cs
@@ -9,14 +9,23 @@ namespace CasinoGames.Core
 {
     public class CasinoCard : ICard
     {
-        public string Name => throw new NotImplementedException();
+        public string Name { get; private set; }
 
-        public int Value => throw new NotImplementedException();
+        public int Value { get; private set; }
 
-        public FacingDirection Facing { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public FacingDirection Facing { get; set; }
 
-        public Sprite FrontImage => throw new NotImplementedException();
+        public Sprite FrontImage { get; private set; }
 
-        public Sprite BackImage => throw new NotImplementedException();
+        public Sprite BackImage { get; private set; }
+
+        public CasinoCard(string name, int value, FacingDirection facing, Sprite frontImage, Sprite backImage)
+        {
+            Name = name;
+            Value = value;
+            Facing = facing;
+            FrontImage = frontImage;
+            BackImage = backImage;
+        }
     }
 }
